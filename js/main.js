6977a3439659623db40f2cadb2cbf49145010d6f
alcasber25 <alcasber25@gmail.com>
2026-04-30 16:58:38 +0200

    inicio de proyecto

---
+++ js/main.js
@@ -0,0 +1,53 @@
+//-- Array de imágenes --//
+
+imagenesViajes = [
+    {
+        id: 1,
+        src: '/assets/images/viajes/viajes-1.jpg',
+        title: 'Mujer en la playa',
+        alt: 'foto de una mujer en hamaca en la playa',
+        tags: ['playa', 'arena', 'palmera', 'mar']
+    },
+    {
+        id: 2,
+        src: '/assets/images/viajes/viajes-2.jpg',
+        title: 'Un muelle en el mar',
+        alt: 'foto de un muelle en el mar',
+        tags: ['playa', 'cielo', 'mar', 'muelle']
+    },
+        {
+        id: 3,
+        src: '/assets/images/viajes/viajes-3.jpg',
+        title: 'Cartel con destinos',
+        alt: 'foto de un poste con un carteles con nombres de ciudades',
+        tags: ['cielo', 'ciudad']
+    },
+        {
+        id: 4,
+        src: '/assets/images/viajes/viajes-4.jpg',
+        title: 'Plaza España en Sevilla',
+        alt: 'foto de la Plaza españa con una porcion del puente',
+        tags: ['plaza', 'monumento', 'palmera', 'ciudad']
+    },
+        {
+        id: 5,
+        src: '/assets/images/viajes/viajes-5.jpg',
+        title: 'Plaza España en Sevilla 2',
+        alt: 'foto de la Plaza españa con una porcion del puente',
+        tags: ['puente', 'monumento', 'farola', 'plaza', 'ciudad']
+    },
+        {
+        id: 6,
+        src: '/assets/images/viajes/viajes-6.jpg',
+        title: 'Camino al tunel por la orilla',
+        alt: 'foto de un camino costero que atraviesa un tunel',
+        tags: ['playa', 'tunel', 'mar']
+    },
+        {
+        id: 7,
+        src: '/assets/images/viajes/viajes-6.jpg',
+        title: 'Un castillo en las alturas',
+        alt: 'foto de un pueblo con casas blancas. Al fondo se ve un castillo.',
+        tags: ['castillo', 'casas', 'montaña', 'campo']
+    },
+]
